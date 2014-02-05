156c914c566134408aa36247612ccccda745ce82
David M. Carr <david@carrclan.us>
2014-02-04 22:01:07 -0500

    add json support using jackson

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,10 +1,18 @@
+import ratpack.jackson.JacksonModule
+import static ratpack.jackson.Jackson.json
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.groovy.Groovy.groovyTemplate
 
 ratpack {
+    modules {
+        register new JacksonModule()
+    }
     handlers {
         get {
             render groovyTemplate("index.html", title: "My Ratpack App")
+        }
+        get("some-json") {
+            render json(user: 1)
         }
         
         assets "public"
